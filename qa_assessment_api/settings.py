c02e008d3f253f46071f63189368205051d32092
Daniel Barra <dbarra@avenuecode.com>
2019-12-04 15:24:25 -0300

    Add recovery token

--- qa_assessment_api/settings.py
+++ qa_assessment_api/settings.py
@@ -133,5 +133,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',)
 }
--- qa_assessment_api/urls.py
+++ qa_assessment_api/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from api.views import user_view, group_view
+from api.views import user_view, group_view, token_view
 
 router = routers.DefaultRouter()
-router.register(r'users', user_view.UserViewSet)
-router.register(r'groups', group_view.GroupViewSet)
+router.register(r'api/users', user_view.UserViewSet)
+router.register(r'api/groups', group_view.GroupViewSet)
 
 urlpatterns = [
     path('_manager/super', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/login', token_view.TokenViewSet.login),
 ]
