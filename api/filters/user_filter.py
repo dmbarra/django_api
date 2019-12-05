fc496392a5b257379919149a3bd8e0a9f7e3cec5
Daniel Barra <dbarra@avenuecode.com>
2019-12-05 16:41:12 -0300

    Limited access for group informations

---
+++ api/filters/user_filter.py
@@ -0,0 +1,8 @@
+import django_filters
+from django.contrib.auth.models import User
+
+
+class UserFilter(django_filters.FilterSet):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'groups', ]
--- api/views/group_view.py
+++ api/views/group_view.py
@@ -1,8 +1,31 @@
-from django.contrib.auth.models import Group
-from rest_framework import viewsets
+from django.contrib.auth.models import Group, User
+from rest_framework import viewsets, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 from api.serializers.group_serializer import GroupSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+    def get_queryset(self):
+        auth_token = self.request.META.get('HTTP_AUTHORIZATION', '').replace('Token ', '')
+        username = Token.objects.get(key=auth_token).user
+        user = User.objects.get(username=username)
+
+        if user.is_superuser:
+            return Group.objects.all()
+        else:
+            return Group.objects.filter(name='invalid')
+
+    def create(self, request):
+        auth_token = request.META.get('HTTP_AUTHORIZATION', '').replace('Token ', '')
+        user = Token.objects.get(key=auth_token).user
+
+        if user.is_superuser:
+            return super(GroupViewSet, self).create(request)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
