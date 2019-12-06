a8074eef0fab0cf424d75eaa3f00b73f2e58f964
Daniel Barra <dbarra@avenuecode.com>
2019-12-06 10:00:22 -0300

    Create specific permission for create user

---
+++ api/permissions/action_based_permission.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import AllowAny
+
+
+class ActionBasedPermission(AllowAny):
+    def has_permission(self, request, view):
+        for klass, actions in getattr(view, 'action_permissions', {}).items():
+            if view.action in actions:
+                return klass().has_permission(request, view)
+        return False
--- api/views/user_view.py
+++ api/views/user_view.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from api.permissions.action_based_permission import ActionBasedPermission
 from api.serializers.user_serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
+    permission_classes = (ActionBasedPermission,)
+    action_permissions = {
+        IsAuthenticated: ['update', 'partial_update', 'destroy', 'list', 'retrieve', ],
+        AllowAny: ['create']
+    }
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        auth_token = self.request.META.get('HTTP_AUTHORIZATION', '').replace('Token ', '')
+        username = Token.objects.get(key=auth_token).user
+        user = User.objects.get(username=username)
+
+        if user.is_superuser:
+            return User.objects.all().order_by('-date_joined')
+        else:
+            return User.objects.filter(username=user.username).order_by('-date_joined')
